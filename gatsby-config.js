f592d72d7956c814a66b2f99d05c070ab65083ce
Andrew Davis <tuf75588@temple.edu>
2018-03-22 12:46:27 -0400

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Default Starter",
+    desc: "A new blog"
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "img",
+        path: `${__dirname}/src/images`
+      }
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp"
+  ]
+};
---
+++ src/components/Header/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Link from "gatsby-link";
+import styled from "styled-components";
+import Img from "gatsby-image";
+
+import logo from "../../images/logo.svg";
+
+const HeaderWrapper = styled.div`
+  background: #524763;
+  margin-bottom: 1.45rem;
+  overflow: hidden;
+  position: relative;
+  height: 70vh;
+  h1 {
+    img {
+      height: 80px;
+    }
+  }
+`;
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+  position: relative;
+  z-index: 2;
+`;
+
+const Header = ({ data }) => (
+  <HeaderWrapper>
+    <HeaderContainer>
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: "white",
+            textDecoration: "none"
+          }}
+        >
+          <img src={logo} alt="Level Up Logo" />
+        </Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </HeaderContainer>
+    <Img
+      style={{
+        position: "absolute",
+        left: 0,
+        top: 0,
+        width: "100%",
+        height: "100%"
+      }}
+      sizes={data.background.sizes}
+    />
+  </HeaderWrapper>
+);
+
+export default Header;
